7b727e5d506ad35abbe92cbd1a8cb3278bdc62ca
daelynj <daelyndbjones@gmail.com>
2019-06-21 10:57:06 -0700

    Run Prettier plugin on all files

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -7,6 +7,6 @@ require 'capybara/rspec'
 RSpec.configure do |config|
   config.include RSpec::FeatureExampleGroup
 
-  config.include Capybara::DSL,           feature: true
+  config.include Capybara::DSL, feature: true
   config.include Capybara::RSpecMatchers, feature: true
 end
