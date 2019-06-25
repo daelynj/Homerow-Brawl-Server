ca7100affe3175912096c1c05e4714497c822b0c
daelynj <daelyndbjones@gmail.com>
2019-06-25 12:02:21 -0700

    Create interactor to fetch all players
    If we separate the business logic from the action that we will soon
    generate we can adhere to the single responsibility principle. So we
    will have our action receive requests from players and have our
    interactor handle what to do with requests.

---
+++ lib/typinggame_server/interactors/players/index.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'hanami/interactor'
+
+module Interactors
+  module Players
+    class Index
+      include Hanami::Interactor
+
+      expose :players
+
+      def initialize(dependencies = {})
+        @players_repository =
+          dependencies.fetch(:repository) { PlayersRepository.new }
+      end
+
+      def call
+        @players = @players_repository.all
+      end
+    end
+  end
+end
