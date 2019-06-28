6e958923c39507211941cfe6dddd835dfe76159a
daelynj <daelyndbjones@gmail.com>
2019-06-28 10:00:43 -0700

    Implement interactor to create a Player entity
    We initialize it with a random UUID provided by securerandom because
    each player will carry a UUID.

--- lib/typinggame_server/interactors/players/create_player.rb
+++ lib/typinggame_server/interactors/players/create_player.rb
@@ -1,10 +1,20 @@
 require 'hanami/interactor'
+require 'securerandom'
 
 module Interactors
   module Players
     class CreatePlayer
       include Hanami::Interactor
-      def call(params); end
+
+      expose :player
+
+      def initialize(repository: PlayerRepository.new)
+        @players_repository = repository
+      end
+
+      def call
+        @player = @players_repository.create(token: SecureRandom.uuid)
+      end
     end
   end
 end
--- spec/typinggame_server/interactors/players/create_player_spec.rb
+++ spec/typinggame_server/interactors/players/create_player_spec.rb
@@ -3,4 +3,16 @@ require 'spec_helper'
 describe Interactors::Players::CreatePlayer do
   let(:repository) { PlayerRepository.new }
   let(:interactor) { described_class.new(repository: repository) }
+
+  context 'good input' do
+    let(:result) { interactor.call }
+
+    it 'succeeds' do
+      expect(result.successful?).to be(true)
+    end
+
+    it 'creates a player' do
+      expect(result.player).to have_attributes(id: Integer)
+    end
+  end
 end
