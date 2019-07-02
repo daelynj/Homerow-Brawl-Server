95d433f2f944f5012102b4ecbaf86e971795f9f2
daelynj <daelyndbjones@gmail.com>
2019-07-02 09:46:34 -0700

    Add action for delete player endpoint
    we quit early if we are given a nil player. An argument can be made that
    by introducing nil at a lower level in the repository's find_by_token
    method that we have now had to add two extra nil checks but I don't
    believe this to be a huge issue at the moment. I am open to other ideas.

--- apps/api/controllers/players/destroy.rb
+++ apps/api/controllers/players/destroy.rb
@@ -4,7 +4,15 @@ module Api
       class Destroy
         include Api::Action
 
-        def call(params)
+        def initialize(interactor: Interactors::Players::DestroyPlayer.new)
+          @interactor = interactor
+        end
+
+        def call(_params)
+          uuid = request.env['HTTP_UUID']
+
+          player = @interactor.call(uuid).player
+          halt 400 && self.status = 400 if player.nil?
         end
       end
     end
