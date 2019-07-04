02b323f79a57707006847444df701de062ccb7e8
daelynj <daelyndbjones@gmail.com>
2019-07-04 13:36:52 -0700

    Add spec for WebsocketConnection middleware
    I got too excited and forgot to write a spec for this when we got the
    middleware working.
    
    I will opt to write a spec for websocket_connection.rb when it has some
    functionality as there's no reason to test puts.

---
+++ spec/lib/middleware/websocket/websocket_connection_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+RSpec.describe WebsocketConnection do
+  let(:app) { double }
+  let(:websocket_connection) { WebsocketConnection.new(app) }
+  let(:env) { { 'rack.upgrade?' => rack_upgrade?, 'rack.upgrade' => nil } }
+
+  describe '#call' do
+    subject { websocket_connection.call(env) }
+
+    context 'when rack.upgrade? is nil' do
+      let(:rack_upgrade?) { nil }
+
+      it 'routes past the websocket middleware' do
+        expect(app).to receive(:call).with(env)
+        subject
+      end
+    end
+
+    context 'when rack.upgrade? contains a :websocket label' do
+      let(:rack_upgrade?) { :websocket }
+
+      it 'upgrades the connection' do
+        subject
+        expect(env['rack.upgrade']).to be_kind_of(WebsocketController)
+      end
+
+      it 'returns a 200 status' do
+        expect(subject).to eq([200, {}, []])
+      end
+    end
+  end
+end
