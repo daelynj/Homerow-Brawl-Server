b12629e328e5fc592cfc578de404e84998034bcf
daelynj <daelyndbjones@gmail.com>
2019-07-10 10:26:47 -0700

    Change instances of client in the Client class
    What is being represented here in what was previously known as client
    is not portrayed clear enough with that variable name as we start to
    interact with Client objects that contain a connection.

--- lib/middleware/websocket/client.rb
+++ lib/middleware/websocket/client.rb
@@ -1,10 +1,10 @@
 module Websocket
   class Client
-    attr_reader :connection_client, :client_attributes
+    attr_reader :connection, :client_attributes
     attr_accessor :position
 
-    def initialize(connection_client: client)
-      @connection_client = connection_client
+    def initialize(connection: client)
+      @connection = connection
       @position = 0
     end
 
