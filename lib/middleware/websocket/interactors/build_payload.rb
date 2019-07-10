e1a2d911ed92cf2b2cda04fc3fe79a77e3b6e1e4
daelynj <daelyndbjones@gmail.com>
2019-07-10 09:39:46 -0700

    Update BuildPayload to use a call method
    We don't want to have any side effects in our initialize methods. Plus,
    we were treating them as a call method by not actually doing anything
    with our instantiated class.

--- lib/middleware/websocket/interactors/build_payload.rb
+++ lib/middleware/websocket/interactors/build_payload.rb
@@ -1,16 +1,9 @@
 module Websocket
   module Interactor
     class BuildPayload
-      attr_reader :payload
-
-      def initialize(clients: clients)
-        @clients = clients
-        @payload = perform_build
-      end
-
-      def perform_build
+      def call(clients: clients)
         players = []
-        @clients.each { |client| players << client.client_attributes }
+        clients.each { |client| players << client.client_attributes }
 
         { 'players' => players }.to_json
       end
--- spec/lib/middleware/websocket/interactors/build_payload_spec.rb
+++ spec/lib/middleware/websocket/interactors/build_payload_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe Websocket::Interactor::BuildPayload do
-  let(:client_1) { Websocket::Client.new(connection_client: nil) }
-  let(:client_2) { Websocket::Client.new(connection_client: nil) }
+  let(:client_1) do
+    Websocket::Client.new(connection_client: double('client').as_null_object)
+  end
+  let(:client_2) do
+    Websocket::Client.new(connection_client: double('client').as_null_object)
+  end
   let(:clients) { [client_1, client_2] }
-  let(:build_payload) { described_class.new(clients: clients) }
 
-  describe '#perform_build' do
-    subject { build_payload.perform_build }
+  describe '#call' do
+    subject { described_class.new.call(clients: clients) }
 
     it 'builds a json payload' do
       expect(subject).to eq('{"players":[{"position":0},{"position":0}]}')
