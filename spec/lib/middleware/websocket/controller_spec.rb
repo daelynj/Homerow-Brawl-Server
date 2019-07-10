b24929c580958651a606470a483e3e3840a2be10
daelynj <daelyndbjones@gmail.com>
2019-07-10 10:56:51 -0700

    Remove ClientInteractor usage from Controller
    We don't actually this need this middleman ClientInteractor as all it
    does is wrap methods around an array of Client objects. It is
    reasonable for the controller to keep track of the list of Client
    objects because it directly receives the connections from Iodine.

--- spec/lib/middleware/websocket/controller_spec.rb
+++ spec/lib/middleware/websocket/controller_spec.rb
@@ -1,43 +1,42 @@
 require 'spec_helper'
 
 RSpec.describe Websocket::Controller do
-  let(:connection) { double }
-  let (:controller) do
-    described_class.new
-  end
-
-  describe '#initialize' do
-    it 'creates a client interactor' do
-      expect(controller.client_interactor).to be_kind_of(
-        Websocket::Interactor::ClientInteractor
-      )
-    end
-  end
+  let(:connection_1) { double('connection').as_null_object }
+  let(:connection_2) { double('connection').as_null_object }
+  let(:controller) { described_class.new }
 
   describe '#on_open' do
-    subject { controller.on_open(connection) }
-
-    it 'tells the interactor to generate a Client' do
-      expect(controller.client_interactor).to receive(:create_client).with(
-        incoming_connection: connection
-      )
-      subject
+    before do
+      controller.on_open(connection_1)
+      controller.on_open(connection_2)
     end
 
-    it 'tells the interactor to update all clients' do
-      expect(controller.client_interactor).to receive(:create_client)
-      subject
+    it 'appends a Client to the client list' do
+      expect(controller.clients[0].connection).to eq(connection_1)
+      expect(controller.clients[1].connection).to eq(connection_2)
+    end
+
+    it 'updates all clients using the write method' do
+      expect(controller.clients[0].connection).to receive(:write)
+      expect(controller.clients[1].connection).to receive(:write)
+
+      controller.on_open(connection_1)
+      controller.on_open(connection_2)
     end
   end
 
   describe '#on_close' do
-    subject { controller.on_close(connection) }
+    before do
+      controller.on_open(connection_1)
+      controller.on_open(connection_2)
+      controller.on_close(connection_2)
+    end
 
-    it 'tells the interactor to remove the incoming client' do
-      expect(controller.client_interactor).to receive(:delete_client).with(
-        incoming_connection: connection
-      )
-      subject
+    it 'removes the incoming client from the client list' do
+      clients = controller.clients
+
+      expect(clients.length).to eq(1)
+      expect(clients.first.connection).to eq(connection_1)
     end
   end
 end
