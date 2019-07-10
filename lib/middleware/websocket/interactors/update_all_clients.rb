3dc16380832913c8b09481e8de670ea358afe7b3
daelynj <daelyndbjones@gmail.com>
2019-07-10 09:41:09 -0700

    Update UpdateAllClients to use a call method
    We don't want to have any side effects in our initialize methods. Plus,
    we were treating them as a call method by not actually doing anything
    with our instantiated class.
    
    We can also afford to make our generate payload method private, as it
    doesn't need to be accessible elsewhere.

--- lib/middleware/websocket/interactors/update_all_clients.rb
+++ lib/middleware/websocket/interactors/update_all_clients.rb
@@ -3,18 +3,15 @@ require './lib/middleware/websocket/interactors/build_payload'
 module Websocket
   module Interactor
     class UpdateAllClients
-      def initialize(clients: clients)
-        @clients = clients
-        @payload = generate_payload
-        perform_update
+      def call(clients: clients)
+        payload = generate_payload(clients: clients)
+        clients.each { |client| client.connection_client.write(payload) }
       end
 
-      def perform_update
-        @clients.each { |client| client.connection_client.write(@payload) }
-      end
+      private
 
-      def generate_payload
-        BuildPayload.new(clients: @clients).payload
+      def generate_payload(clients: clients)
+        BuildPayload.new.call(clients: clients)
       end
     end
   end
--- spec/lib/middleware/websocket/interactors/update_all_clients_spec.rb
+++ spec/lib/middleware/websocket/interactors/update_all_clients_spec.rb
@@ -8,25 +8,16 @@ RSpec.describe Websocket::Interactor::UpdateAllClients do
     Websocket::Client.new(connection_client: double('client').as_null_object)
   end
   let(:clients) { [client_1, client_2] }
-  let(:update_all_clients) { described_class.new(clients: clients) }
+  let(:update_all_clients) { described_class.new }
 
   describe '#perform_update' do
-    subject { update_all_clients.perform_update }
+    subject { update_all_clients.call(clients: clients) }
 
     it 'calls the write method for each client' do
       update_all_clients
       expect(client_1.connection_client).to receive(:write)
       expect(client_2.connection_client).to receive(:write)
       subject
-    end
-  end
-
-  describe '#generate_payload' do
-    let(:expected_payload) { '{"players":[{"position":0},{"position":0}]}' }
-    subject { update_all_clients.generate_payload }
-
-    it 'receives a payload' do
-      expect(subject).to eq(expected_payload)
     end
   end
 end
