1040cfc9f6128436b199135e39621bb1ff006bf2
daelynj <daelyndbjones@gmail.com>
2019-07-15 13:41:35 -0700

    Remove stub and generate Player entity for testing

--- spec/lib/middleware/websocket/interactors/client_creation_payload_spec.rb
+++ spec/lib/middleware/websocket/interactors/client_creation_payload_spec.rb
@@ -4,18 +4,13 @@ RSpec.describe Websocket::Interactor::ClientCreationPayload do
   let(:client_1) do
     Websocket::Client.new(connection: double('connection').as_null_object)
   end
-  let(:client_2) do
-    Websocket::Client.new(connection: double('connection').as_null_object)
-  end
-  let(:clients) { [client_1, client_2] }
 
   describe '#call' do
     subject { described_class.new.call(client: client_1) }
-
-    before { allow(client_1.player).to receive(:id).and_return(5) }
+    before { client_1.generate_player }
 
     it 'builds a json payload' do
-      expect(subject).to eq('{"id":5}')
+      expect(subject).to eq("{\"id\":#{client_1.player.id}}")
     end
   end
 end
