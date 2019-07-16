2337fe75532e555e6d43be1ea3bef5fbd52a4d8e
daelynj <daelyndbjones@gmail.com>
2019-07-16 15:35:10 -0700

    Add interactor to fetch all Room entities
    We want to be able to fetch all the Room entities in the database with
    an interactor so that we separate business logic from the controller
    that will inevitably need to achieve this.

---
+++ spec/lib/typinggame_server/interactors/rooms/fetch_all_rooms_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Interactors::Rooms::FetchAllRooms do
+  let(:repository) { RoomRepository.new }
+  let(:interactor) { described_class.new(repository: repository) }
+
+  before do
+    repository.create(id: '1')
+    repository.create(id: '2')
+  end
+
+  context 'good input' do
+    let(:result) { interactor.call }
+
+    it 'succeeds' do
+      expect(result.successful?).to be(true)
+    end
+
+    it 'fetches all rooms' do
+      expect(result.rooms.first.id).to be(1)
+      expect(result.rooms.length).to be(2)
+    end
+  end
+end
