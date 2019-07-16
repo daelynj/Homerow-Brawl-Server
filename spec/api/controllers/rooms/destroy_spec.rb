d801bb4f5dd1e40ea8ce89564f78e61a025a2295
daelynj <daelyndbjones@gmail.com>
2019-07-16 16:59:16 -0700

    Generate Destroy Room action
    We want to be able to visit an endpoint to destroy a Room entity so that
    we can have old rooms be deleted

---
+++ spec/api/controllers/rooms/destroy_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe Api::Controllers::Rooms::Destroy, type: :action do
+  let(:repository) { RoomRepository.new }
+  let(:interactor) do
+    Interactors::Rooms::DestroyRoom.new(repository: repository)
+  end
+  let(:action) { described_class.new(interactor: interactor) }
+
+  context 'nil input' do
+    let(:params) { Hash[id: nil] }
+
+    it 'is unsuccessful' do
+      response = action.call(params)
+      expect(response[0]).to eq(400)
+    end
+  end
+
+  context 'good input' do
+    let(:params) { Hash[id: '1'] }
+    before { repository.create(id: '1', players: 0) }
+
+    it 'is successful' do
+      response = action.call(params)
+      expect(response[0]).to eq(200)
+      expect(repository.find(1)).to eq(nil)
+    end
+  end
+end
