0eebd069bad325f4484ea46d1d3297f31e2ca4fb
daelynj <daelyndbjones@gmail.com>
2019-07-17 16:23:34 -0700

    Update HandleMessage to update rooms
    When a message comes in we no longer want to update all clients, we only
    want to update the clients that are associated with the room that that
    message is coming from.

--- spec/lib/middleware/websocket/interactors/handle_message_spec.rb
+++ spec/lib/middleware/websocket/interactors/handle_message_spec.rb
@@ -7,16 +7,22 @@ RSpec.describe Websocket::Interactor::HandleMessage do
   let(:client_2) do
     Websocket::Client.new(connection: double('connection').as_null_object)
   end
+  let(:room) { Websocket::Room.new(id: 1) }
   let(:clients) { [client_1, client_2] }
 
   describe '#call' do
     describe 'a position update' do
       let(:data) { { 'position' => 5 } }
+
       subject do
-        described_class.new.call(data: data, client: client_1, clients: clients)
+        described_class.new.call(data: data, room: room, client: client_1)
       end
 
       before do
+        room.add_client(client: client_1)
+        room.add_client(client: client_2)
+        client_1.room_id = 1
+        client_2.room_id = 1
         allow(client_1.player).to receive(:id)
         allow(client_2.player).to receive(:id)
       end
@@ -26,7 +32,7 @@ RSpec.describe Websocket::Interactor::HandleMessage do
         expect(client_1.position).to eq(5)
       end
 
-      it('updates all clients with the race status') do
+      it('updates all clients in the clients room with the race status') do
         expect(client_1.connection).to receive(:write)
         expect(client_2.connection).to receive(:write)
         subject
@@ -36,15 +42,19 @@ RSpec.describe Websocket::Interactor::HandleMessage do
     describe 'the countdown has started' do
       let(:data) { { 'countdown' => true } }
       subject do
-        described_class.new.call(data: data, client: client_1, clients: clients)
+        described_class.new.call(data: data, room: room, client: client_1)
       end
 
       before do
+        room.add_client(client: client_1)
+        room.add_client(client: client_2)
+        client_1.room_id = 1
+        client_2.room_id = 1
         allow(client_1.player).to receive(:id)
         allow(client_2.player).to receive(:id)
       end
 
-      it 'updates all clients with the timer status' do
+      it 'updates all clients in the clients room with the timer status' do
         expect(client_1.connection).to receive(:write).with(
           '{"countdown":true}'
         )
