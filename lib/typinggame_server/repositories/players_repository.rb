69dfffbde242c7fa2cc0d70c020ad8604710eca7
daelynj <daelyndbjones@gmail.com>
2019-07-24 15:32:49 -0700

    Remove default parameter in PlayerRepository
    This default parameter doesn't make sense as it's not actually a thing.

--- lib/typinggame_server/repositories/players_repository.rb
+++ lib/typinggame_server/repositories/players_repository.rb
@@ -1,5 +1,5 @@
 class PlayerRepository < Hanami::Repository
-  def find_by_token(token: token)
+  def find_by_token(token:)
     players.where(token: token).to_a.first
   rescue => e
     return nil
