704169e305df2014704469aa93fb9a374195dc39
daelynj <daelyndbjones@gmail.com>
2019-07-24 15:28:55 -0700

    Update controller
    We no longer have the concept of Client and Room objects. By using
    Iodine's pub/sub implementation we can avoid concurrency issues and
    subscribe new connections to the room provided in the env hash from the
    request.
    
    Because of this we can also send updates to specific rooms that
    connections have subscribed to upon connecting.

--- lib/middleware/websocket/controller.rb
+++ lib/middleware/websocket/controller.rb
@@ -1,31 +1,16 @@
-require './lib/middleware/websocket/client'
-require './lib/middleware/websocket/interactors/updates/race_update'
-require './lib/middleware/websocket/interactors/updates/client_creation_update'
-require './lib/middleware/websocket/room'
+require './lib/middleware/websocket/interactors/handle_new_connection'
+require './lib/middleware/websocket/interactors/handle_message'
 require 'json'
 
 module Websocket
   class Controller
-    attr_reader :clients
-    attr_accessor :rooms
-
-    def initialize
-      @clients = []
-      @rooms = []
-    end
-
     def on_open(connection)
-      client = generate_client(connection: connection)
-
-      race_update.call(room: find_room(id: client.room_id))
+      Interactor::HandleNewConnection.new.call(connection: connection)
     end
 
     def on_message(connection, data)
-      client = find_client(connection: connection)
-      room = find_room(id: client.room_id)
-
       Interactor::HandleMessage.new.call(
-        data: JSON.parse(data), room: room, client: client
+        data: JSON.parse(data), connection: connection
       )
     end
 
@@ -34,50 +19,9 @@ module Websocket
     end
 
     def on_close(connection)
-      client = find_client(connection: connection)
-      client.delete_player
-      @clients -= [client]
-
-      race_update.call(room: find_room(id: client.room_id))
-    end
-
-    private
-
-    def generate_client(connection:)
-      client = Client.new(connection: connection)
-      client.generate_player
-      @clients << client
-      setup_room(client: client)
-
-      Interactor::ClientCreationUpdate.new.call(client: @clients.last)
-
-      return client
-    end
-
-    def setup_room(client:)
-      connection_path = client.connection.env['PATH_INFO'][1..].to_i
-      room = find_room(id: connection_path)
-
-      if room.nil?
-        room = Room.new(id: connection_path)
-        @rooms << room
-      end
-
-      room.add_client(client: client)
-
-      return room
-    end
-
-    def find_client(connection:)
-      @clients.detect { |client| client.connection == connection }
-    end
-
-    def race_update
-      @race_update ||= Interactor::RaceUpdate.new
-    end
-
-    def find_room(id:)
-      @rooms.detect { |room| room.id == id }
+      #find the Player and delete them
+      #find the associated table entry and delete it
+      #check if the room is empty - if yes, delete it
     end
   end
 end
