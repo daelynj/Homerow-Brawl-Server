4b2422f5d9ed061b7e4f04d5edd49501d2a18fed
daelynj <daelyndbjones@gmail.com>
2019-07-31 11:08:42 -0700

    Fix add_column addition to users

--- db/migrations/20190731163609_create_users.rb
+++ db/migrations/20190731163609_create_users.rb
@@ -1,7 +1,9 @@
 Hanami::Model.migration do
   change do
     alter_table :users do
-      add_column :uuid, default: Hanami::Model::Sql.function(:uuid_generate_v4)
+      add_column :uuid,
+                 'uuid',
+                 default: Hanami::Model::Sql.function(:uuid_generate_v4)
     end
   end
 end
