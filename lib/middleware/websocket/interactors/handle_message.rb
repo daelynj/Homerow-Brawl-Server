c2789cc910f5a83037b66b65a03157638b61bd6c
daelynj <daelyndbjones@gmail.com>
2019-08-02 13:42:15 -0700

    Update HandleMessage service object
    We now require that all requests be verified through a UUID. Moving
    forward we will implement a time check for expired UUIDs.
    
    We perform all of the same updates as before, but now check that a
    requests UUID is valid so that we are only performing valid race
    updates.

--- lib/middleware/websocket/interactors/handle_message.rb
+++ lib/middleware/websocket/interactors/handle_message.rb
@@ -1,18 +1,31 @@
 require './lib/middleware/websocket/interactors/updates/race_update'
 require './lib/middleware/websocket/interactors/updates/timer_update'
+require './lib/middleware/websocket/interactors/handle_new_connection'
 require './lib/typinggame_server/interactors/players_rooms/update_player_room'
+require './lib/typinggame_server/interactors/players/fetch_player'
 
 module Websocket
   module Interactor
     class HandleMessage
       def call(data:, connection:)
         room_id = connection.env['PATH_INFO'][1..].to_i
+        uuid = data['uuid']
 
-        if data.key?('position')
-          update_player_position(data: data, room_id: room_id)
-          RaceUpdate.new.call(connection: connection, room_id: room_id)
-        elsif data.key?('countdown')
-          TimerUpdate.new.call(connection: connection, room_id: room_id)
+        if uuid && data.length == 1
+          Interactor::HandleNewConnection.new.call(
+            uuid: uuid, connection: connection
+          )
+        end
+
+        if data.key?('position') && data.key?('uuid')
+          if player_verified?(uuid: uuid)
+            update_player_position(data: data, room_id: room_id)
+            RaceUpdate.new.call(connection: connection, room_id: room_id)
+          end
+        elsif data.key?('countdown') && data.key?('uuid')
+          if player_verified?(uuid: uuid)
+            TimerUpdate.new.call(connection: connection, room_id: room_id)
+          end
         end
       end
 
@@ -22,6 +35,12 @@ module Websocket
         Interactors::PlayersRooms::UpdatePlayerRoom.new.call(
           data: data, room_id: room_id
         )
+      end
+
+      def player_verified?(uuid:)
+        player = Interactors::Players::FetchPlayer.new.call(uuid: uuid).player
+
+        player.nil? ? false : true
       end
     end
   end
