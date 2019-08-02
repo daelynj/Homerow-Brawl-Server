fe2183aaba0619386ba0a9db81f022190023421b
daelynj <daelyndbjones@gmail.com>
2019-08-02 13:04:52 -0700

    Add table join for Player_rooms and Players
    We need to be able to query off the player_rooms table to retrieve
    a Player entities name along with their position in the race. This
    allows us to display names client side.

--- spec/lib/typinggame_server/repositories/player_room_repository_spec.rb
+++ spec/lib/typinggame_server/repositories/player_room_repository_spec.rb
@@ -1,8 +1,30 @@
 require 'spec_helper'
 
 RSpec.describe PlayerRoomRepository, type: :repository do
-  let(:player_1) { Interactors::Players::CreatePlayer.new.call.player }
-  let(:player_2) { Interactors::Players::CreatePlayer.new.call.player }
+  let(:player_attributes_1) { { 'id' => 1, 'name' => 'octane' } }
+  let(:team_1) { { 'id' => 'X0rlA1' } }
+  let(:access_token_1) { 'fdgdfg934kdn9gf09fgh8' }
+
+  let(:player_attributes_2) { { 'id' => 2, 'name' => 'dominus' } }
+  let(:team_2) { { 'id' => 'X0klA3' } }
+  let(:access_token_2) { 'fdgdfg908g9n9gf09fgh8' }
+
+  let(:player_1) do
+    Interactors::Players::CreatePlayer.new.call(
+      player_attributes: player_attributes_1,
+      team: team_1,
+      access_token: access_token_1
+    )
+      .player
+  end
+  let(:player_2) do
+    Interactors::Players::CreatePlayer.new.call(
+      player_attributes: player_attributes_2,
+      team: team_2,
+      access_token: access_token_2
+    )
+      .player
+  end
   let(:room) { Interactors::Rooms::CreateRoom.new.call.room }
   let(:repository) { PlayerRoomRepository.new }
   let(:create_player_room) { Interactors::PlayersRooms::CreatePlayerRoom.new }
@@ -40,6 +62,24 @@ RSpec.describe PlayerRoomRepository, type: :repository do
         expect(subject.player_id).to eq(player_1.id)
         expect(subject.room_id).to eq(room.id)
       end
+    end
+  end
+
+  describe '#find_players_names_positions' do
+    subject { repository.find_players_names_positions(room_id: room.id) }
+
+    before do
+      create_player_room.call(player_id: player_1.id, room_id: room.id)
+      create_player_room.call(player_id: player_2.id, room_id: room.id)
+    end
+
+    it 'joins the players table and player_rooms table' do
+      expect(subject).to eq(
+        [
+          { name: 'octane', player_id: player_1.id, position: 0 },
+          { name: 'dominus', player_id: player_2.id, position: 0 }
+        ]
+      )
     end
   end
 end
