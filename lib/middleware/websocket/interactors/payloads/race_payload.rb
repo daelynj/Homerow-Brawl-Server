aea2d802b72f000354053cd46eb96102411ae015
daelynj <daelyndbjones@gmail.com>
2019-08-02 13:29:56 -0700

    Update RacePayload to send player name
    Using the fetch_players_names_positions interactor we can retrieve
    all the data we need to supply a client with a race update that contains
    player names.

--- lib/middleware/websocket/interactors/payloads/race_payload.rb
+++ lib/middleware/websocket/interactors/payloads/race_payload.rb
@@ -1,22 +1,33 @@
-require './lib/typinggame_server/interactors/players_rooms/fetch_players_rooms'
+require './lib/typinggame_server/interactors/players_rooms/fetch_players_names_positions'
 
 module Websocket
   module Interactor
     class RacePayload
       def call(room_id:)
-        player_room_records =
-          Interactors::PlayersRooms::FetchPlayersRooms.new.call(
-            room_id: room_id
-          )
-            .player_room_records
+        players_names_positions =
+          fetch_players_names_positions(room_id: room_id)
 
         players = []
 
-        player_room_records.each do |player|
-          players << { 'id' => player.player_id, 'position' => player.position }
+        players_names_positions.each do |player|
+          players <<
+            {
+              'id' => player[:player_id],
+              'name' => player[:name],
+              'position' => player[:position]
+            }
         end
 
         { 'players' => players }.to_json
+      end
+
+      private
+
+      def fetch_players_names_positions(room_id:)
+        Interactors::PlayersRooms::FetchPlayersNamesPositions.new.call(
+          room_id: room_id
+        )
+          .players_names_positions
       end
     end
   end
