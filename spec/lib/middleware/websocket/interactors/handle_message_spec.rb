f6455b4ac09035f916cdce7fa47f8d5ba39990e9
daelynj <daelyndbjones@gmail.com>
2019-08-02 13:39:53 -0700

    Update HandleMessage service object
    We now only perform game updates upon receiving an authenticated
    request. To verify requests we ask that they contain a valid UUID.
    
    We still perform all the same updates as before, we just verify their
    UUID.
    
    We will have to add some checks here that check for expired UUIDs. This
    will come soon.

--- spec/lib/middleware/websocket/interactors/handle_message_spec.rb
+++ spec/lib/middleware/websocket/interactors/handle_message_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe Websocket::Interactor::HandleMessage do
-  let(:player) { Interactors::Players::CreatePlayer.new.call.player }
+  let(:player_attributes) { { 'id' => 1, 'name' => 'octane' } }
+  let(:team) { { 'id' => 'X0klA3' } }
+  let(:access_token) { 'fdgdfg908g9n9gf09fgh8' }
+  let(:player) do
+    Interactors::Players::CreatePlayer.new.call(
+      player_attributes: player_attributes,
+      team: team,
+      access_token: access_token
+    )
+      .player
+  end
   let(:room) { Interactors::Rooms::CreateRoom.new.call.room }
   let(:env) { { 'PATH_INFO' => "/#{room.id}" } }
   let(:connection) { double('connection', env: env) }
@@ -15,8 +25,34 @@ RSpec.describe Websocket::Interactor::HandleMessage do
       create_player_room_record.call(player_id: player.id, room_id: room.id)
     end
 
-    context 'when the client sends a position update' do
-      let(:data) { { 'id' => player.id, 'position' => 30 } }
+    context 'when the client sends their uuid' do
+      let(:data) { { 'uuid' => player.uuid } }
+      subject { handle_message.call(data: data, connection: connection) }
+
+      it 'subscribes the player to a room, and performs player join and race updates' do
+        expect(connection).to receive(:subscribe).with("#{room.id}")
+        expect(connection).to receive(:write).with(
+          "{\"id\":#{player.id},\"name\":\"octane\"}"
+        )
+        expect(connection).to receive(:publish).with(
+          "#{room.id}",
+          "{\"players\":[{\"id\":#{
+            player.id
+          },\"name\":\"octane\",\"position\":0}]}"
+        )
+        subject
+      end
+    end
+
+    context 'when the client sends a position update with a proper UUID' do
+      let(:data) do
+        {
+          'id' => player.id,
+          'uuid' => player.uuid,
+          'name' => 'octane',
+          'position' => 30
+        }
+      end
       subject { handle_message.call(data: data, connection: connection) }
 
       it 'updates the players position' do
@@ -36,15 +72,34 @@ RSpec.describe Websocket::Interactor::HandleMessage do
       it 'sends all players in the room a race update' do
         expect(connection).to receive(:publish).with(
           "#{room.id}",
-          "{\"players\":[{\"id\":#{player.id},\"position\":30}]}"
+          "{\"players\":[{\"id\":#{
+            player.id
+          },\"name\":\"octane\",\"position\":30}]}"
         )
 
         subject
       end
     end
 
+    context 'when the client sends a position update with an improper UUID' do
+      let(:data) do
+        {
+          'id' => player.id,
+          'uuid' => 'improper uuid',
+          'name' => 'octane',
+          'position' => 30
+        }
+      end
+
+      subject { handle_message.call(data: data, connection: connection) }
+
+      it 'fails to perform the update' do
+        expect(subject).to be(nil)
+      end
+    end
+
     context 'when a player sends a countdown update' do
-      let(:data) { { 'countdown' => true } }
+      let(:data) { { 'uuid' => player.uuid, 'countdown' => true } }
 
       subject { handle_message.call(data: data, connection: connection) }
 
