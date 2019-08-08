8bc6d6f4073f99f0cadd7d447aed4b3db3d6ca41
daelynj <daelyndbjones@gmail.com>
2019-08-08 10:09:30 -0700

    Implement HandleCountdownUpdate service
    This service doesn't take much to work but to stay conventional it makes
    sense to also separate this into a handler as we have done with a
    player joining and a race update.

---
+++ spec/lib/middleware/websocket/interactors/handlers/handle_coountdown_update_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe Websocket::Interactor::Handler::HandleCountdownUpdate do
+  let(:connection) { double('connection') }
+
+  describe '#call' do
+    subject { described_class.new.call(connection: connection, room_id: 5) }
+
+    it 'sends a countdown update to all connections of a room in a JSON payload' do
+      expect(connection).to receive(:publish).with(
+        '5',
+        '{"type":"countdown","countdown":true}'
+      )
+      subject
+    end
+  end
+end
