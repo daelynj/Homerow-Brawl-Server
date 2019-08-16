7d1f88bba6a78b140a8a0de0eec763db618911e7
daelynj <daelyndbjones@gmail.com>
2019-08-16 13:27:15 -0700

    Create action and view for players_rooms#show
    This will be the endpoint to hit for retrieving global stats about a
    player that we can use to display stats on the client.

---
+++ apps/api/controllers/players_rooms/show.rb
@@ -0,0 +1,12 @@
+module Api
+  module Controllers
+    module PlayersRooms
+      class Show
+        include Api::Action
+
+        def call(params)
+        end
+      end
+    end
+  end
+end
---
+++ apps/api/views/players_rooms/show.rb
@@ -0,0 +1,9 @@
+module Api
+  module Views
+    module PlayersRooms
+      class Show
+        include Api::View
+      end
+    end
+  end
+end
