3b73b5029024447ffeb716373d2985c45b82664f
daelynj <daelyndbjones@gmail.com>
2019-08-19 16:57:12 -0700

    Add interactor to fetch a players global stats
    Given a players' UUID we want to retrieve all player_room records for
    that player and build a payload that we can send to the client to
    display a players global stats. To achieve this we use this interactor.

--- lib/typinggame_server/interactors/players_rooms/fetch_player_global_stats.rb
+++ lib/typinggame_server/interactors/players_rooms/fetch_player_global_stats.rb
@@ -5,13 +5,23 @@ module Interactors
     class FetchPlayerGlobalStats
       include Hanami::Interactor
 
-      expose :player_global_stats
+      expose :player_stats
 
       def initialize(repository: PlayerRoomRepository.new)
         @player_room_repository = repository
       end
 
-      def call; end
+      def call(uuid:)
+        player = Players::FetchPlayer.new.call(uuid: uuid).player
+
+        return nil if player.nil?
+
+        records =
+          @player_room_repository.find_player_room_records(player_id: player.id)
+
+        @player_stats =
+          BuildPlayerGlobalStats.new.call(records: records).player_stats
+      end
     end
   end
 end
